16a36ed11135f4ed168237595e91a6f1ac0780b5
Ginger Cuevas <ginger.cuevas@gmail.com>
2018-11-24 21:18:44 -0800

    new react code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    barGraph: [],
+    results: [],
+    rates: '',
+    base: '',
+  }
+  componentDidMount() {
+    this.fetchCurrencies();
+  }
+
+  fetchCurrencies = (base = 'USD') => {
+    fetch(`https://api.exchangeratesapi.io/latest?symbols=USD,EUR,GBP,AUD&base=${base}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log("got data", data);
+      const rateValues = Object.values(data.rates);
+      const highestRate = Math.max(...rateValues);
+      this.setState({
+        rates: data.rates,
+        highestRate: highestRate,
+      });
+    });
+  }
+
+  onSubmit = () => {
+    this.fetchCurrencies();
+
+  }
+
+  updateBase = (ev) => {
+    this.setState({
+      base: ev.target.value,
+    });
+    this.fetchCurrencies(ev.target.value);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="">
+          <ul className="bar-graph">
+            {
+              Object.keys(this.state.rates).map(key => (
+                <li className="bar" key={key} style={{height: ((this.state.rates[key] / this.state.highestRate) * 100) + '%'}}>
+                  {this.state.rates[key]}
+                  <br></br>  
+                  {key}
+                </li>
+              ))
+  
+            }
+          </ul>
+          <select onChange={this.updateBase} value={this.state.base}>
+              <option value="USD">US Dollar</option>
+              <option value="AUD">Australian Dollar</option>
+              <option value="GBP">British Pound</option>
+          </select>
+        </div>
       </div>
     );
   }
 }
-
 export default App;
